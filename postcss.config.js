d9a249f20de877f5cb2fd6240d5c24394c4f2bc3
Ssteier2016 <109993596+Ssteier2016@users.noreply.github.com>
2025-11-06 22:03:28 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  // Utilizamos el formato de objeto, que es el recomendado
-  // para las versiones modernas de PostCSS y Tailwind CSS (PostCSS 8+)
-  plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-  }
-}
+      plugins: {
+        'tailwindcss': {},
+        'autoprefixer': {},
+      }
+    }
